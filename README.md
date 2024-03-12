a076016d888c878c3fa61dfd12c1011936e30b3e
ZIMUQIN-L <68744060+ZIMUQIN-L@users.noreply.github.com>
2024-03-12 22:03:39 +0100

    [feat]: init pages

---
+++ README.md
@@ -0,0 +1,72 @@
+### 功能页面
+
+#### 首页
+
+/pages/home/
+
+#### 房源详情
+
+/pages/details/
+
+#### 房源信息post
+
+/pages/repost/
+
+#### 搜索页面
+
+/pages/search/
+
+#### 用户profile页面（未完成）
+
+/pages/user/
+
+### 数据格式
+
+```
+{
+    "_id":"2da1518365f0a5b502053dee5ebc13e6",
+    "_openid":"owGKZ60jXYIzshdSNVVNd4QSVLoY",
+    "contact":"111",
+    "end_date":"2024-03-21",
+    "houseSurrounding":
+        {
+        "近中超":true,
+        "近地铁":false,
+        "近景点":false
+        },
+    "images":
+        [
+            "cloud://cloud1-8gb29x2pbe14a4f8.636c-cloud1-8gb29x2pbe14a4f8-1324366677/userPhoto/1710269861084.jpg",
+            "cloud://cloud1-8gb29x2pbe14a4f8.636c-cloud1-8gb29x2pbe14a4f8-1324366677/userPhoto/1710269868874.jpg"
+        ],
+    "preference":
+        {
+            "限女生":false,
+            "限男生":false,
+            "不限":false,
+            "可吸烟":false,
+            "宠物友好":false,
+            "换宿":false,
+            "换洗床具":false,
+            "短租":true
+        },
+    "start_date":"2024-03-07",
+    "capacity":5,
+    "description":"xx",
+    "houseSetting":
+        {
+            "沙发":true,
+            "洗衣机":false,
+            "独立卫浴":true,
+            "独立厨房":false,
+            "空调":false,
+            "WIFI":false,
+            "冰箱":false,
+            "暖气":false
+        },
+    "houseType":"apartment",
+    "location":"阿姆斯特丹",
+    "ownerTarget":"巴塞罗那"
+}
+```
+
---
+++ pages/details/details.js
@@ -0,0 +1,114 @@
+Page({
+  data: {
+    detailInfo: {},
+    active: 0,
+    goods: {
+      title: 'House ID',
+      location: 'Tiny home in Madrid, Spain',
+      guests: '2 guests | 1 bedroom | 1 bath',
+      onwer: 'WX: 123456',
+      price: 2680,
+      formatPrice: '',
+      express: '免运费',
+      remain: 19,
+      thumb:
+        'https://img.yzcdn.cn/vant/cat.jpeg',
+    },
+  },
+  onLoad() {
+    this.setData({
+      active: 0
+    })
+    var that  = this;
+    var detailId = wx.getStorageSync('DetailId');
+    const db = wx.cloud.database()
+    const _ = db.command
+    db.collection('HouseInfo').where({
+      _id: detailId
+    })
+    .get({
+      success: function(res) {
+        console.log(res);
+        that.setData({
+          detailInfo: res.data[0]
+        })
+      }
+    })
+  },
+
+  onClickCart() {
+    wx.navigateTo({
+      url: '/pages/cart/index',
+      success: () => {},
+      error: () => {
+        wx.showToast({
+          icon: 'none',
+          title: '打开购物车失败',
+        });
+      },
+    });
+  },
+
+  onClickUser() {
+    wx.navigateTo({
+      url: '/pages/home/home',
+      success: () => {},
+      error: () => {
+        wx.showToast({
+          icon: 'none',
+          title: '打开个人中心失败',
+        });
+      },
+    });
+  },
+
+  onClickButton() {
+    wx.showToast({
+      title: '暂无后续逻辑~',
+      icon: 'none',
+    });
+  },
+
+  sorry() {
+    wx.showToast({
+      title: '暂无后续逻辑~',
+      icon: 'none',
+    });
+  },
+  onGoHome() {
+    wx.navigateTo({
+      url: '/pages/home/home',
+      success: () => {},
+      error: () => {
+        wx.showToast({
+          icon: 'none',
+          title: '打开个人中心失败',
+        });
+      },
+    });
+  },
+  onGoProfile() {
+    wx.navigateTo({
+      url: '/pages/user/index',
+      success: () => {},
+      error: () => {
+        wx.showToast({
+          icon: 'none',
+          title: '打开个人中心失败',
+        });
+      },
+    });
+  },
+  onGoPost() {
+    wx.navigateTo({
+      url: '/pages/repost/repost',
+      success: () => {},
+      error: () => {
+        wx.showToast({
+          icon: 'none',
+          title: '打开个人中心失败',
+        });
+      },
+    });
+  },
+});
