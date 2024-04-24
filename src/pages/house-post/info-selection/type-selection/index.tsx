7c52e59b312f2f6ece5a8cecfad40ba389d56c89
xiangddang <935725453@qq.com>
2024-04-24 16:18:25 +0800

    save all

--- src/pages/house-post/info-selection/type-selection/index.tsx
+++ src/pages/house-post/info-selection/type-selection/index.tsx
@@ -2,9 +2,9 @@ import { View, Text } from '@tarojs/components';
 import CustomFullScreenDialog from '@components/CustomFullScreenDialog';
 import './index.scss';
 
-const TypeSelection = ({ isOpen }) => {
+const TypeSelection = ({ onClose }) => {
   return (
-    <CustomFullScreenDialog title='选择床型' isOpen={isOpen}>
+    <CustomFullScreenDialog title='选择床型' onClose={onClose}>
       <View>
         <Text>这里是床型选择的地方</Text>
       </View>
