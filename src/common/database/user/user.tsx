49e77e0c43c30a9f27538fc2d370683f923a6051
ZIMUQIN-L <68744060+ZIMUQIN-L@users.noreply.github.com>
2024-04-25 22:57:59 +0200

    add user info search

--- src/common/database/user/user.tsx
+++ src/common/database/user/user.tsx
@@ -1,43 +1,17 @@
-export const userInfoSearch = async (
-  nickName = '',
-  avatarUrl = '',
-  openId = '',
-  skip = 0,
-) => {
-  const db = wx.cloud.database();
-  const _ = db.command;
-  return new Promise((resolve, reject) => {
-    db.collection('UserInfo')
-      .where({
-        location: db.RegExp({
-          regexp: '^.*' + location + '.*',
-        }),
-        nickName: nickName,
-        avatarUrl: avatarUrl,
-        openId: openId,
-      })
-      .skip(skip)
-      .limit(10)
-      .get({
-        success: function (res) {
-          resolve(res.data);
-        },
-      });
-  });
-};
+// 检查用户是否存在并获取用户信息
+export const userOpenidSearch = async ( userOpenid
+  ) => {
+    const db = wx.cloud.database();
+    return new Promise((resolve, reject) => {
+      db.collection('UserInfo')
+        .where({
+         userOpenid: userOpenid
+        })
+        .get({
+          success: function (res) {
+            resolve(res.data);
+          },
+        });
+    });
+  };
 
-// 用户界面详情
-export const UserInfoSearch = async openId => {
-  const db = wx.cloud.database();
-  return new Promise((resolve, reject) => {
-    db.collection('UserInfo')
-      .where({
-        _id: openId,
-      })
-      .get({
-        success: function (res) {
-          resolve(res.data[0]);
-        },
-      });
-  });
-};
