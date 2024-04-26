1c62d9beb1f3e9ea8824b0f975262c15ba18beac
ZIMUQIN-L <68744060+ZIMUQIN-L@users.noreply.github.com>
2024-04-26 21:09:01 +0200

    add user agreement

--- src/common/database/user/user.tsx
+++ src/common/database/user/user.tsx
@@ -1,36 +1,36 @@
 // 检查用户是否存在并获取用户信息
-export const userInfoSearch = async ( userOpenid
-  ) => {
-    const db = wx.cloud.database();
-    return new Promise((resolve, reject) => {
-      db.collection('UserInfo')
-        .where({
-         userOpenid: userOpenid
-        })
-        .get({
-          success: function (res) {
-            resolve(res.data);
-          },
-        });
-    });
-  };
+export const userInfoSearch = async userOpenid => {
+  const db = wx.cloud.database();
+  return new Promise((resolve, reject) => {
+    db.collection('UserInfo')
+      .where({
+        userOpenid: userOpenid,
+      })
+      .get({
+        success: function (res) {
+          resolve(res.data);
+        },
+      });
+  });
+};
 
 // 添加用户信息
 export const userInfoAdd = async (userOpenid, userNickName, userAvatarUrl) => {
-    const db = wx.cloud.database();
-    console.log(userOpenid, userNickName, userAvatarUrl);
-    return new Promise((resolve, reject) => {
-    db.collection('UserInfo').add({
+  const db = wx.cloud.database();
+  console.log(userOpenid, userNickName, userAvatarUrl);
+  return new Promise((resolve, reject) => {
+    db.collection('UserInfo')
+      .add({
         data: {
-            userOpenid: userOpenid,
-            nickName: userNickName,
-            avatarUrl: userAvatarUrl,
-            userDes: '',
-        }
-    }).then(res => {
+          userOpenid: userOpenid,
+          nickName: userNickName,
+          avatarUrl: userAvatarUrl,
+          userDes: '',
+        },
+      })
+      .then(res => {
         resolve(res.errMsg);
         console.log(res);
-    })
-});
-}
-
+      });
+  });
+};
--- src/store/GlobalStore/index.ts
+++ src/store/GlobalStore/index.ts
@@ -9,20 +9,20 @@ class GlobalStore {
   }
 
   get userInfo() {
-      const globalUserInfo : UserItemProps = {
-        _id: this._userInfo._id,
-        _openid: this._userInfo._openid,
-        avatarUrl: this._userInfo.avatarUrl,
-        nickName: this._userInfo.nickName,
-        userDes: this._userInfo.userDes,
-        userOpenid: this._userInfo.userOpenid,
-      }
-      console.log(globalUserInfo);
-      return globalUserInfo;
+    const globalUserInfo: UserItemProps = {
+      _id: this._userInfo._id,
+      _openid: this._userInfo._openid,
+      avatarUrl: this._userInfo.avatarUrl,
+      nickName: this._userInfo.nickName,
+      userDes: this._userInfo.userDes,
+      userOpenid: this._userInfo.userOpenid,
+    };
+    console.log(globalUserInfo);
+    return globalUserInfo;
   }
 
   set userInfo(updateUserInfo: UserItemProps) {
-      this._userInfo = updateUserInfo;
+    this._userInfo = updateUserInfo;
   }
 
   get currentTab() {
