6bd67f1f2b668d5f7c21d7cfb4b3c392bd70b338
qinxinren1 <qinxin.ren18@gmail.com>
2024-05-04 15:17:06 +0200

    欧洲地址级联选择
    目前只做了欧洲

--- src/pages/home/search-section/index.tsx
+++ src/pages/home/search-section/index.tsx
@@ -5,18 +5,108 @@ import { useState, createContext, useRef, useEffect } from 'react';
 // import SearchIcon from '@assets/images/search.svg';
 import CustomDateRangePicker from '@components/CustomDateRangePicker';
 import { RightBottomArrow, SearchIcon } from '../../../utils/cloudIcons';
-
+import addressData from './addressData';
+import {AtList, AtListItem, AtTextarea} from "taro-ui";
+//import options from '../../../common/database/world_city/addressJson';
+interface Child {
+  id: string;
+  pid: string;
+  path: string;
+  level: string;
+  name: string;
+  name_en: string;
+  name_pinyin: string;
+  code: string | null;
+  childrens?: Child[];
+}
+const selectorAddress = addressData
 const SearchCard = ({ onDestinationChange, onDateChange, onClickSearch }) => {
-  const [region, setRegion] = useState('欧洲');
   // 先只保留欧洲
-  // const regions = ['欧洲', '亚洲', '北美', '南美', '非洲', '大洋洲'];
-  const regions = ['欧洲'];
 
-  const handleRegionChange = e => {
-    const index = e.detail.value;
-    const selectedRegion = regions[index];
-    setRegion(selectedRegion);
-  };
+  
+    const [address, setAddress] = useState('请选择地址')
+    //三级联动
+    const [customArray, setCustomArray] = useState(selectorAddress)
+    const [customIndex, setCustomIndex] = useState([0, 0, 0])
+    let [onlyArray, setOnlyArray] = useState([[], [], []])
+  
+    useEffect(() => {
+      const data = {
+        customArray: customArray,
+        customIndex: customIndex,
+        onlyArray: onlyArray,
+      };
+      for (let i = 0; i < data.customArray.length; i++) {
+        // @ts-ignore
+        data.onlyArray[0].push(customArray[i].name);
+      }
+      for (let j = 0; j < data.customArray[customIndex[0]].cityList.length; j++) {
+        // @ts-ignore
+        data.onlyArray[1].push(customArray[customIndex[0]].cityList[j].name);
+      }
+      for (let k = 0; k < customArray[customIndex[0]].cityList[customIndex[1]].areaList.length; k++) {
+        // @ts-ignore
+        data.onlyArray[2].push(customArray[customIndex[0]].cityList[customIndex[1]].areaList[k]);
+      }
+      console.log(data.customArray)
+      setCustomArray(data.customArray)
+      setCustomIndex(data.customIndex)
+      setOnlyArray(data.onlyArray)
+    })
+  
+    //多列选择
+    const bindCustomPickerColumnChange = (e) => {
+      const newCustomArray = selectorAddress
+      const newCustomIndex = customIndex
+      const newOnlyArray = onlyArray
+  
+      newCustomIndex[e.detail.column] = e.detail.value;
+      // console.log(e.detail)
+      // console.log(onlyArray);
+  
+      const searchColumn = () => {
+        for (let i = 0; i < newCustomArray.length; i++) {
+          let arr1 = [];
+          let arr2 = [];
+          if (i == newCustomIndex[0]) {
+            for (let j = 0; j < newCustomArray[i].cityList.length; j++) {
+              // @ts-ignore
+              arr1.push(newCustomArray[i].cityList[j].name);
+              if (j == newCustomIndex[1]) {
+                for (let k = 0; k < newCustomArray[i].cityList[j].areaList.length; k++) {
+                  // @ts-ignore
+                  arr2.push(newCustomArray[i].cityList[j].areaList[k]);
+                }
+                newOnlyArray[2] = arr2;
+              }
+            }
+            newOnlyArray[1] = arr1;
+          }
+        };
+      }
+  
+      switch (e.detail.column) {
+        case 0:
+          newCustomIndex[1] = 0;
+          newCustomIndex[2] = 0;
+          searchColumn();
+          break;
+        case 1:
+          newCustomIndex[2] = 0;
+          searchColumn();
+          break;
+      }
+      setOnlyArray([[],[],[]])
+      setCustomIndex(newCustomIndex)
+    }
+
+    const addressOnChange = (e) => {
+      const indexArr = e.detail.value
+      // console.log(customArray[indexArr[0]].cityList[indexArr[1]].areaList[indexArr[2]])
+  
+      const addressText = `${customArray[indexArr[0]].name}${customArray[indexArr[0]].cityList[indexArr[1]].areaList[indexArr[2]]}`
+      setAddress(addressText)
+    }
 
   const [startDate, setStartDate] = useState<Date>();
   const [endDate, setEndDate] = useState<Date>();
@@ -36,18 +126,15 @@ const SearchCard = ({ onDestinationChange, onDateChange, onClickSearch }) => {
 
   return (
     <View className='search-card'>
-      <View className='search-first-line'>
-        <Picker
-          className='region-input'
-          mode='selector'
-          range={regions}
-          onChange={handleRegionChange}
-        >
-          <View className='picker'>
-            <Text>{region}</Text>
-            <Image src={RightBottomArrow} className='right-bottom-arrow' />
-          </View>
-        </Picker>
+      <View className='search-first-line'>    
+      <Picker mode='multiSelector' range={onlyArray} onChange={addressOnChange} value={customIndex} onColumnChange={bindCustomPickerColumnChange.bind(this)}>
+          <AtList>
+            <AtListItem
+              extraText={address}
+            >
+            </AtListItem>
+          </AtList>
+        </Picker>    
         <View className='vertical-line' />
         <Input
           className='destination-input'
