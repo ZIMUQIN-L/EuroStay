82855f02ce13cad077016bde0dcc55aba4494116
ZIMUQIN-L <68744060+ZIMUQIN-L@users.noreply.github.com>
2024-05-10 10:53:03 +0200

    change home initial search and image folder for uploading

--- src/packageUser/user-edit/index.tsx
+++ src/packageUser/user-edit/index.tsx
@@ -6,7 +6,7 @@ import { UserItemProps } from '@utils/interfaces';
 import './index.scss';
 import Taro from '@tarojs/taro';
 import GlobalStore from '@store/GlobalStore';
-import { cloudImageUpload } from '../../common/database/cloudstorage/files';
+import { cloudAvatarUpload } from '../../common/database/cloudstorage/files';
 import { userInfoUpdate } from '../../common/database/user/user';
 import CustomTabBar from '@components/CustomTabBar';
 
@@ -43,10 +43,12 @@ const Index = () => {
           title: '上传中',
           mask: true,
         });
-        cloudImageUpload(tempFilePaths[0]).then((uploadedImagePath: string) => {
-          setUserAvatarUrl(uploadedImagePath);
-          Taro.hideLoading();
-        });
+        cloudAvatarUpload(tempFilePaths[0]).then(
+          (uploadedImagePath: string) => {
+            setUserAvatarUrl(uploadedImagePath);
+            Taro.hideLoading();
+          },
+        );
       },
       fail: function (err) {
         console.log(err);
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -9,6 +9,7 @@ import HouseItem from './house-item';
 import { houseInfoSearch } from '../../common/database/house/house';
 import { HouseItemProps } from '@utils/interfaces';
 import SearchAndFilter from './search-and-filter';
+import { formatToday } from '@utils/dateUtil';
 
 const Index = () => {
   Taro.useShareAppMessage(() => {
@@ -54,11 +55,12 @@ const Index = () => {
   const [demoData, setDemoData] = useState<HouseItemProps[]>([]);
 
   const fetchInitialData = () => {
-    houseInfoSearch('阿姆', '2024-03-24', '2024-03-24').then(
-      (houseData: HouseItemProps[]) => {
-        setDemoData(houseData); // Set demo data to the fetched initial list
-      },
-    );
+    const today = formatToday();
+    console.log(today);
+    houseInfoSearch('', today, today).then((houseData: HouseItemProps[]) => {
+      console.log(houseData);
+      setDemoData(houseData); // Set demo data to the fetched initial list
+    });
   };
 
   // Effect to fetch data on mount
