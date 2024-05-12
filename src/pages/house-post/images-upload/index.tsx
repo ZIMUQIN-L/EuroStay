7461ff55a53a3c266fa24ec0d7aa0e3129fdb38f
xiangddang <935725453@qq.com>
2024-05-12 15:43:00 +0800

    优化图片上传错误抓取 && 图片删除后异步删除数据库中的数据

--- src/pages/house-post/images-upload/index.tsx
+++ src/pages/house-post/images-upload/index.tsx
@@ -1,70 +1,81 @@
 import { View, Image, Text } from '@tarojs/components';
 import './index.scss';
-// import UploadIcon from '@assets/images/upload-icon.svg';
 import Taro from '@tarojs/taro';
 import { cloudImageUpload } from '@common/database/cloudstorage/files';
 
 const ImagesUpload = ({ images, onUploadImage, onDeleteImage }) => {
   const hasImages = Array.isArray(images) && images.length > 0;
 
+  const showFailureToast = (message) => {
+    Taro.showToast({
+      title: message,
+      icon: 'error',
+      duration: 2000,
+    });
+  }
+
   const handleUploadImage = () => {
+    Taro.showLoading({
+      title: '图片选择中',
+      mask: true,
+    });
+
     Taro.chooseImage({
-      count: 1, // 默认9
+      count: 1,
       sizeType: ['original', 'compressed'],
       sourceType: ['album', 'camera'],
-      success: function (res) {
-        const tempFilePaths = res.tempFilePaths;
-
-        // 为了不上传大量图片至服务器端在测试时使用本地图片，请在上线时删除下面两行代码
-        onUploadImage(tempFilePaths);
-        return;
-
-        // 压缩照片
-        Taro.compressImage({
-          src: tempFilePaths[0],
+    }).then(async res => {
+      if (res.tempFilePaths.length > 0) {
+        const tempFilePath = res.tempFilePaths[0];
+        const compressedImage = Taro.compressImage({
+          src: tempFilePath,
           quality: 50,
-          success(res) {
-            const compressedImagePath = res.tempFilePath;
-            Taro.showLoading({
-              title: '图片上传中',
-              mask: true,
-            });
-            cloudImageUpload(compressedImagePath)
-              .then((uploadedImagePath: string) => {
-                onUploadImage(uploadedImagePath);
-                Taro.hideLoading();
-              })
-              .catch(err => {
-                Taro.showToast({
-                  title: '图片上传失败',
-                  icon: 'none',
-                  duration: 2000,
-                });
-                Taro.hideLoading();
-              });
-          },
-          fail(err) {
-            Taro.showToast({
-              title: '图片压缩失败',
-              icon: 'none',
-              duration: 2000,
-            });
-          },
+        })
+        Taro.showLoading({
+          title: '图片上传中',
+          mask: true,
         });
-      },
-      fail: function (err) {
-        Taro.showToast({
-          title: '选择图片失败',
-          icon: 'none',
-          duration: 2000,
+        compressedImage.then(res => {
+          return cloudImageUpload(res.tempFilePath);
+        })
+        .then(uploadedImagePath => {
+          onUploadImage(uploadedImagePath);
+          Taro.hideLoading();
+        })
+        .catch(err => {
+          showFailureToast('上传图片失败');
+          Taro.hideLoading();
         });
-      },
+      }
+    }).catch(err => {
+      Taro.hideLoading();
+      if (err.errMsg === 'chooseImage:fail cancel') {
+        return;
+      }
+      showFailureToast('选择图片失败');
     });
   };
 
   const handleDeleteImage = imageUrl => {
     onDeleteImage(imageUrl);
+    deleteImageFile(imageUrl);
   };
+
+  async function deleteImageFile(tempFilePath) {
+    try {
+      await Taro.cloud.deleteFile({
+        fileList: [tempFilePath],
+      });
+      Taro.showToast({
+        title: '删除图片成功',
+        icon: 'success',
+        duration: 2000,
+      });
+    }
+    catch (err) {
+      showFailureToast('删除图片失败');
+    }
+  }
 
   return (
     <View className='upload-part'>
