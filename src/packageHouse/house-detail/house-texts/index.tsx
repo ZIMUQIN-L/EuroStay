c15c0d115af0c096aab25a38c93d806aae637edd
Lindsay <lindsay.chen@yale.edu>
2024-06-30 12:29:12 -0700

    profile

--- src/packageHouse/house-detail/house-texts/index.tsx
+++ src/packageHouse/house-detail/house-texts/index.tsx
@@ -3,8 +3,40 @@ import { HouseDetailItemProps } from '@utils/interfaces';
 import './index.scss';
 import { DateIcon, CapacityIcon, LocationIcon } from '@utils/cloudIcons';
 import Taro from '@tarojs/taro';
+import { UserDetailInfoItemProps } from '@utils/interfaces';
+
+// mock data
+const userInfo: UserDetailInfoItemProps = {
+  _id: 'user-001',
+  _openid: 'openid-001',
+  userOpenid: 'user-openid-001',
+  nickName: '偷心小白菜',
+  userDes: 'A friendly and outgoing person who loves to explore new places and meet new people.',
+  avatarUrl: 'https://via.placeholder.com/80',
+  userLocation: 'Milan, Italy',
+  guestRating: 4.8,
+  guestRatingNumber: 25,
+  hostRating: 4.7,
+  hostRatingNumber: 18,
+  gender: 'female',
+  tags: ['adventurous', 'friendly', 'outgoing'],
+  verified: {
+    student: true,
+    gov: true
+  },
+  aboutMe: {
+    interests: 'Swimming, Movies, Skiing',
+    major: 'Computer Science',
+    languages: 'English, Italian, Chinese',
+    skills: 'Coding, Cooking, Photography',
+    funFact: 'I have visited 30 countries and counting!',
+    visitedCountries: 'Italy, France, Germany, USA, China, Japan',
+    serviceProvided: 'I can offer a cozy place to stay and a local tour around Milan.'
+  }
+};
 
 const toHostDetail = () => {
+  Taro.setStorageSync('userDetail', userInfo);
   Taro.navigateTo({
     url: '/packageUser/user-detail/index',
   });
--- src/packageUser/user-detail/index.tsx
+++ src/packageUser/user-detail/index.tsx
@@ -6,25 +6,34 @@ import Taro from '@tarojs/taro';
 import { RightBottomArrow } from '@utils/cloudIcons';
 import GlobalStore from '@store/GlobalStore';
 
-// const UserDetail = () => {
-  // const [userInfo, setUserInfo] = useState<UserItemProps>(GlobalStore.userInfo);
-  // useEffect(() => {
-  //   const globalUserInfo: UserItemProps = GlobalStore.userInfo;
-  //   setUserInfo(globalUserInfo);
-  //   console.log('User attributes:', globalUserInfo);
-  // }, []);
-
-
-
-  // const UserDetail: React.FC<UserItemProps> = user => {
-  // const [userInfo, setUserInfo] = useState<UserItemProps>(user) ;
-  // useEffect(() => {
-  //   const userData: UserDetailInfoItemProps = Taro.getStorageSync('userDetail');
-  //   if (userData) {
-  //     setUserInfo(userData);
-  //   }
-  //   console.log('User attributes:', user);
-  // }, [user]);
+const userData: UserDetailInfoItemProps = {
+  _id: 'user-001',
+  _openid: 'openid-001',
+  userOpenid: 'user-openid-001',
+  nickName: '偷心小白菜',
+  userDes: 'A friendly and outgoing person who loves to explore new places and meet new people.',
+  avatarUrl: 'https://via.placeholder.com/80',
+  userLocation: 'Milan, Italy',
+  guestRating: 4.8,
+  guestRatingNumber: 25,
+  hostRating: 4.7,
+  hostRatingNumber: 18,
+  gender: 'female',
+  tags: ['INTP', '意大利米兰', '米兰理工大学', 'superrrrrrrlongggggggggggggg'],
+  verified: {
+    student: true,
+    gov: true
+  },
+  aboutMe: {
+    interests: 'Swimming, Movies, Skiing',
+    major: 'Computer Science',
+    languages: 'English, Italian, Chinese',
+    skills: 'Coding, Cooking, Photography',
+    funFact: 'I have visited 30 countries and counting!',
+    visitedCountries: 'Italy, France, Germany, USA, China, Japan',
+    serviceProvided: 'I can offer a cozy place to stay and a local tour around Milan.'
+  }
+};
 
   const UserDetail: React.FC= () => {
     const [userInfo, setUserInfo] = useState<UserDetailInfoItemProps | null>(null);
@@ -42,35 +51,31 @@ import GlobalStore from '@store/GlobalStore';
 
   return (
     <View>
-      {/* <View style={{ width: '100%' }}>
+      <View className="profile-container">
+        <View className="profile-background" />
         <View className="profile-header">
-          <View>
-            <Image src={userInfo.avatarUrl} className="avatar-img" />
+          <Image src={userData.avatarUrl} className="profile-image" />
+          <View className="info">
+            <Text className="profile-name">
+            {userData.nickName}<Text className="badge" >实名认证</Text>
+            </Text>
+            <View className="badges">
+              {userData.tags.map((tag, index) => (
+                <Text key={index} className="badge-item">{tag}</Text>
+              ))}
+            </View>
+            <Text className="description">我的简介：{userData.userDes}</Text>
+            <View className="stats">
+              <Text>房东评分: 4.5 颗星</Text>
+              <Text>房客评分: 4.6 颗星</Text>
+              <Text>获赞与收藏: 100 次</Text>
+            </View>
           </View>
-          <View className="user-texts">
-            <View className="title">{userInfo.nickName}</View>
+          
           </View>
         </View>
-      </View> */}
 
-      <View className="profile-header">
-        <Image src={userInfo.avatarUrl} className="profile-image" />
-        <View className="info">
-          <Text className="profile-name">
-          {userInfo.nickName}<Text style={{ color: 'green' }}>实名认证</Text>
-          </Text>
-          <View className="badges">
-            <Text>INTP</Text>
-            <Text>意大利米兰</Text>
-            <Text>米兰理工大学</Text>
-          </View>
-          <View className="stats">
-            <Text>房东评分: 4.5 颗星</Text>
-            <Text>房客评分: 4.6 颗星</Text>
-            <Text>获赞与收藏: 100 次</Text>
-          </View>
-        </View>
-      </View>
+      
       <View className="tabs">
         <View className="tab active">概况</View>
         <View className="tab">供宿</View>
