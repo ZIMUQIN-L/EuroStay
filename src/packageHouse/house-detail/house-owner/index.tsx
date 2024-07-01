e8443d3e8680b21ba5658061b0367edab17c0d9c
Lindsay <lindsay.chen@yale.edu>
2024-07-01 11:50:16 -0700

    host info

--- src/packageHouse/house-detail/house-owner/index.tsx
+++ src/packageHouse/house-detail/house-owner/index.tsx
@@ -18,6 +18,37 @@ const HouseOwner: React.FC<HouseDetailItemProps> = house => {
     });
   };
 
+  // mock data owner
+  const onwer: UserDetailInfoItemProps = {
+    _id: 'user-001',
+    _openid: 'openid-001',
+    userOpenid: 'user-openid-001',
+    nickName: '偷心小白菜',
+    userDes: '欢迎和我进行换宿体验～',
+    avatarUrl: 'https://via.placeholder.com/80',
+    userLocation: 'Milan, Italy',
+    guestRating: 4.8,
+    guestRatingNumber: 25,
+    hostRating: 4.7,
+    hostRatingNumber: 18,
+    gender: 'female',
+    tags: ['INTP', '意大利米兰', '米兰理工大学'],
+    verified: {
+      student: true,
+      gov: true,
+    },
+    aboutMe: {
+      interests: 'Swimming, Movies, Skiing',
+      major: 'Computer Science',
+      languages: 'English, Italian, Chinese',
+      skills: 'Coding, Cooking, Photography',
+      funFact: 'I have visited 30 countries and counting!',
+      visitedCountries: 'Italy, France, Germany, USA, China, Japan',
+      serviceProvided:
+        'I can offer a cozy place to stay and a local tour around Milan.',
+    },
+  };
+
   useEffect(() => {
     userInfoSearch(house._openid).then(
       (ownerInfo: UserDetailInfoItemProps[]) => {
@@ -32,8 +63,17 @@ const HouseOwner: React.FC<HouseDetailItemProps> = house => {
         <View className='text-container'>
           <Text className='title'>房东信息</Text>
         </View>
-        <View className='contact-button' onClick={toHostDetail}>
+        {/* <View className='contact-button' onClick={toHostDetail}>
           <Text className='contact-text'>房东信息</Text>
+        </View> */}
+        <View className="parent-container">
+          <View className="profile-card">
+            <View className="profile-info">
+              <Image src={onwer.avatarUrl} className="profile-image" />
+              <Text className="profile-name">{onwer.nickName}</Text>
+            </View>
+            <Text className="host-homepage-button" onClick={toHostDetail}>房主主页</Text>
+          </View>
         </View>
       </View>
     </View>
