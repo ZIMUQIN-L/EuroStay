c5227df8272cc25d210f893ef0bdeef934f375e5
xiangddang <935725453@qq.com>
2024-07-03 13:01:41 -0400

    point-details

--- src/packageUser/my-points/index.tsx
+++ src/packageUser/my-points/index.tsx
@@ -1,15 +1,10 @@
-import { View, Text, Image } from '@tarojs/components';
+import { View, Text } from '@tarojs/components';
 import { observer } from 'mobx-react';
 import './index.scss';
-import { PointIcon } from '@utils/cloudIcons';
 import Taro from '@tarojs/taro';
+import PointsInfo from './points-info';
 
 const Index = () => {
-  const navigateToDetails = () => {
-    Taro.navigateTo({
-      url: '../../packageUser/point-details/index',
-    });
-  };
 
   const navigateToPost = () => {
     Taro.navigateTo({
@@ -29,21 +24,13 @@ const Index = () => {
     });
   };
 
+  
+
   return (
     <View className='points'>
-      <View className='my-points'>
-        <View className='my-points-points'>
-          <Text style={{ marginLeft: '5%' }}>我的积分</Text>
-          <Image src={PointIcon} />
-          <Text className='my-points-value'>0</Text>{' '}
-          {/* GlobalStore.userInfo.point */}
-        </View>
-        <View className='my-points-details'>
-          <Text className='details-button' onClick={navigateToDetails}>
-            明细
-          </Text>
-        </View>
-      </View>
+      
+      
+      <PointsInfo Detail={false} />
 
       <View className='get-points'>
         <View className='get-points-header'>
---
+++ src/packageUser/point-details/points-table/index.tsx
@@ -0,0 +1,34 @@
+import { View, Text } from '@tarojs/components';
+import './index.scss';
+
+const PointsTable = ({ data }) => {
+  return (
+    <View className='points-table'>
+      <View className='table-header'>
+        <Text className='table-cell'>时间</Text>
+        <Text className='table-cell'>类型</Text>
+        <Text className='table-cell'>积分</Text>
+        <Text className='table-cell'>余额</Text>
+      </View>
+      {data && data.length > 0 ? (
+        data.map((item, index) => (
+          <View className='table-row' key={index}>
+            <View className='table-cell'>
+              {item.date}
+              <Text className='time'>{item.time}</Text>
+            </View>
+            <Text className='table-cell'>{item.type}</Text>
+            <Text className='table-cell'>{item.points}</Text>
+            <Text className='table-cell'>{item.balance}</Text>
+          </View>
+        ))
+      ) : (
+        <View className='no-data'>
+          <Text>没有更多数据了</Text>
+        </View>
+      )}
+    </View>
+  );
+};
+
+export default PointsTable;
