e18da470031b2fa15427ce388fbc4e595d03adce
xiangddang <935725453@qq.com>
2024-07-05 11:05:05 -0400

    my-activity tabbar

--- src/packageActivity/my-activities/index.tsx
+++ src/packageActivity/my-activities/index.tsx
@@ -1,73 +1,36 @@
 import { observer } from 'mobx-react';
-import { DefaultHouse } from '@utils/cloudIcons';
-import { View, Text, Image } from '@tarojs/components';
-import { useEffect, useState } from 'react';
-import GlobalStore from '@store/GlobalStore';
-import { HouseItemProps, UserItemProps } from '@utils/interfaces';
-import './index.scss';
-import Taro from '@tarojs/taro';
-import { userHouseInfoSearch } from '@common/database/user/user';
+import { View, Text } from '@tarojs/components';
 
-const houses = [
-  {
-    id: '1',
-    image: DefaultHouse,
-    title: '宽敞大床房',
-    destination: '意大利罗马',
-    capacity: 2,
-    gender: '仅限女生',
-    likes: 20,
-  },
-  {
-    id: '2',
-    image: DefaultHouse,
-    destination: '意大利米兰',
-    title: '现代风格套房',
-    capacity: 3,
-    gender: '性别不限',
-    likes: 40,
-  },
-];
+import './index.scss';
+import { useState } from 'react';
 
 const Index = () => {
-  const [user, setUser] = useState<UserItemProps>(GlobalStore.userInfo);
-  const [houseList, setHouseList] = useState<HouseItemProps[]>([]);
-  useEffect(() => {
-    const demoUser: UserItemProps = GlobalStore.userInfo;
-    setUser(demoUser);
-    userHouseInfoSearch(demoUser._openid).then(
-      (houseData: HouseItemProps[]) => {
-        setHouseList(houseData); // Update demoData state with the fetched data
-      },
-    );
-  }, [GlobalStore.userInfo]);
+  const [currentTab, setCurrentTab] = useState('initiated');
 
-  const handleHouseClick = houseId => {
-    // Taro.navigateTo({
-    //   // url: `/packageHouse/house-edit/index?id=${houseId}`,
-    // });
+  const isActive = tabName => {
+    return currentTab === tabName ? 'active' : '';
   };
+
   return (
-    <View className='house-grid'>
-      {houseList.map(house => (
-        <View
-          key={house._id}
-          className='house-card'
-          onClick={() => handleHouseClick(house._id)}
-        >
-          <Image
-            src={house.images.length > 0 ? house.images[0] : DefaultHouse}
-            mode='aspectFill'
-          />
-          <Text className='house-title'>{house.location}</Text>
-          <View className='house-info'>
-            <Text>{house.capacity}人</Text>
-            <View className='house-likes'>
-              {/* <Text>{house.likes}</Text> */}
-            </View>
-          </View>
-        </View>
-      ))}
+    <View className='tab-bar'>
+      <View
+        className={isActive('initiated')}
+        onClick={() => setCurrentTab('initiated')}
+      >
+        <Text className={`text ${isActive('initiated')}`}>我发起的</Text>
+      </View>
+      <View
+        className={isActive('registered')}
+        onClick={() => setCurrentTab('registered')}
+      >
+        <Text className={`text ${isActive('registered')}`}>我报名的</Text>
+      </View>
+      <View
+        className={isActive('favorited')}
+        onClick={() => setCurrentTab('favorited')}
+      >
+        <Text className={`text ${isActive('favorited')}`}>我收藏的</Text>
+      </View>
     </View>
   );
 };
