e302a01d43563177e5f976c64115dcb400aab32b
Lindsay <lindsay.chen@yale.edu>
2024-10-30 17:56:11 +0100

    traveler system

--- src/packageUser/traveler-system/index.tsx
+++ src/packageUser/traveler-system/index.tsx
@@ -1,128 +1,59 @@
+
 import { View, Text } from '@tarojs/components';
-import { observer } from 'mobx-react';
-import './index.scss';
 import Taro from '@tarojs/taro';
-import PointsInfo from './points-info';
-import { UserDetailInfoItemProps, UserItemProps } from '@utils/interfaces';
-import GlobalStore from '@store/GlobalStore';
-import React, { useState, useEffect } from 'react';
-import { userInfoSearch } from '@common/database/user/user';
+import './index.scss';
 
-const Index = () => {
-  const [currentUser, setCurrentUser] = useState<UserDetailInfoItemProps>();
-
-  useEffect(() => {
-    userInfoSearch(GlobalStore.userInfo._openid).then(
-      (ownerInfo: UserDetailInfoItemProps[]) => {
-        setCurrentUser(ownerInfo[0]);
-      },
-    );
-  }, []);
-
-  Taro.useShareAppMessage(res => {
-    return {
-      title: 'EuroStay欧洲换宿',
-      path: '/pages/login/index',
-    };
-  });
-
-  const navigateToPost = () => {
-    Taro.navigateTo({
-      url: '../../packageHouse/house-post/index',
-    });
-  };
-
-  const navigateToComment = () => {
-    Taro.navigateTo({
-      url: '../../packageUser/my-accommodation/index?tab=toComment',
-    });
-  };
-
-  const navigateToProfile = () => {
-    Taro.navigateTo({
-      url: `../../packageUser/user-edit/index?id=${currentUser?._openid}`,
-    });
-  };
-
+const ExplorerSystem = () => {
   return (
-    <View className='points'>
-      <PointsInfo Detail={false} currentUserDetail={currentUser} />
-
-      <View className='get-points'>
-        <View className='get-points-header'>
-          <Text>如何获取积分</Text>
-        </View>
-        <View className='get-points-list'>
-          <View className='get-points-item'>
-            <Text>发布更多房源</Text>
-            <View className='get-button' onClick={navigateToPost}>
-              <Text>去发布</Text>
-            </View>
-          </View>
-          <View className='line-div'></View>
-
-          <View className='get-points-item'>
-            <Text>发布更多评论</Text>
-            <View className='get-button' onClick={navigateToComment}>
-              <Text>去评论</Text>
-            </View>
-          </View>
-          <View className='line-div'></View>
-
-          <View className='get-points-item'>
-            <Text>完善个人信息</Text>
-            <View className='get-button' onClick={navigateToProfile}>
-              <Text>去完善</Text>
-            </View>
-          </View>
+    <View className='explorer-system'>
+      {/* Account Section */}
+      <View className='account-section'>
+        <Text className='section-title'>帐篷背景</Text>
+        <Text className='section-link' onClick={() => Taro.navigateTo({ url: '/pages/rules/index' })}>
+          规则说明
+        </Text>
+        <View className='icon-group'>
+          <View className='icon-item'>徽章</View>
+          <View className='icon-item large'>角色</View>
+          <View className='icon-item'>世界地图</View>
         </View>
       </View>
 
-      <View className='points-rules'>
-        <View className='section-title'>
-          <Text>积分规则</Text>
+      {/* Progress Bar Section */}
+      <View className='progress-section'>
+        <Text className='section-title'>成长进度条</Text>
+      </View>
+
+      {/* Explorer Benefits Section */}
+      <View className='benefits-section'>
+        <Text className='section-title'>一级探险家权益</Text>
+        <Text className='section-link' onClick={() => Taro.navigateTo({ url: '/pages/benefits-comparison/index' })}>
+          权益对比
+        </Text>
+        <View className='icon-group'>
+          <View className='icon-item' />
+          <View className='icon-item' />
+          <View className='icon-item' />
+          <View className='icon-item' />
         </View>
+      </View>
 
-        <View className='points-section'>
-          <Text className='points-subtitle'>初始积分：</Text>
-          <Text className='points-value'>10分</Text>
-        </View>
-
-        <View className='line-div'></View>
-
-        <View className='points-section'>
-          <Text className='points-subtitle'>获取积分：</Text>
-          <View className='points-list'>
-            <View className='points-item'>
-              <Text>发布房源：+10分</Text>
-            </View>
-            <View className='points-item'>
-              <Text>完善个人信息：每增加一个板块 +5分</Text>
-            </View>
-            <View className='points-item'>
-              <Text>进行身份认证（学生/实名）：+20分</Text>
-            </View>
-            <View className='points-item'>
-              <Text>评价房东/房客：+5分</Text>
-            </View>
-          </View>
-        </View>
-
-        <View className='line-div'></View>
-
-        <View className='points-section'>
-          <Text className='points-subtitle'>消耗积分：</Text>
-          <View className='points-list'>
-            <View className='points-item'>
-              <Text>
-                报名参加活动：依据活动需要消耗的积分数量确定消耗积分值
-              </Text>
-            </View>
-          </View>
+      {/* Growth Tasks Section */}
+      <View className='tasks-section'>
+        <Text className='section-title'>成长任务</Text>
+        <Text className='section-link' onClick={() => Taro.navigateTo({ url: '/pages/growth-details/index' })}>
+          成长明细
+        </Text>
+        <View className='task-list'>
+          <View className='task-item' />
+          <View className='task-item' />
+          <View className='task-item' />
+          <View className='task-item' />
+          <View className='task-item' />
         </View>
       </View>
     </View>
   );
 };
 
-export default observer(Index);
+export default ExplorerSystem;
