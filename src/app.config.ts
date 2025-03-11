975fceb7151b3f2255770c66e9ec393f61bd8575
shannonmeng333 <shannon.meng@eventeny.com>
2025-03-10 22:22:03 -0400

    Revert "message showing multi kinds of exmaple"
    This reverts commit 0519dcaa134afdc8cefa9953b489ec8a6c76ff14.

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,8 +1,6 @@
 export default {
   lazyCodeLoading: 'requiredComponents',
-  pages: [
-    'pages/home/index',
-  ],
+  pages: ['pages/home/index'],
   subPackages: [
     // {
     //   root: 'packageUser/',
@@ -16,12 +14,6 @@ export default {
     //   root: 'packageActivity/',
     //   pages: [],
     // },
-    {
-      root: 'packageMessage',
-      pages: [
-        'message-detail/index',
-      ],
-    }
   ],
   window: {
     backgroundTextStyle: 'light',
--- src/packageMessage/message-detail/index.config.js
+++
@@ -1,6 +0,0 @@
-// packageMessage/message-list/index.config.js
-export default {
-    navigationBarTitleText: '消息列表',
-    navigationBarBackgroundColor: '#ffffff',
-  }
-  
