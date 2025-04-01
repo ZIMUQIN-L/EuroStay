8c60762344b28c52a54e818e8c31362a7e0a618c
ZIMUQIN-L <68744060+ZIMUQIN-L@users.noreply.github.com>
2025-04-01 22:25:48 +0200

    [fix]

--- src/components/TabBar/index.tsx
+++ src/components/TabBar/index.tsx
@@ -91,7 +91,33 @@ const TabBar: React.FC<TabBarProps> = ({ onWorldSelected, setIsShowPostModal, is
           return;
     }
       else {
-        setIsShowPostModal(true);
+        const response = Taro.request({
+            url: 'https://api.eurostay.co/app/esuser/getUserCompleteInfo',
+            method: 'POST',
+            header: {
+              'token': GlobalStore.userInfo.token,
+              'Content-Type': 'application/json'
+            },
+            success: (res) => {
+              if (res.data.result.email != "" && res.data.result.email != null) {
+                setIsShowPostModal(true);
+              }
+              else {
+                Taro.showModal({
+                    title: '请先认证邮箱哦~',
+                    content: '请先认证邮箱方便guest联系您时进行提醒呀~',
+                    success: function (res) {
+                      if (res.confirm) {
+                        Taro.navigateTo({
+                          url: '/packageUser/user-editing/index',
+                        });
+                      }
+                    }
+                  });
+              }
+            }
+          });
+        // setIsShowPostModal(true);
       }
       return;
     }
