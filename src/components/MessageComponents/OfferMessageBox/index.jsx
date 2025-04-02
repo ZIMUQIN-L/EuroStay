fa8c0cf276a5fbd154daf58ab727491c548c0664
ZIMUQIN-L <68744060+ZIMUQIN-L@users.noreply.github.com>
2025-04-02 18:51:23 +0200

    [fix]

--- src/components/MessageComponents/OfferMessageBox/index.jsx
+++ src/components/MessageComponents/OfferMessageBox/index.jsx
@@ -20,7 +20,7 @@ const OfferMessageBox = (props) => {
   } else {
     // 左侧消息（通常表示"对方"或"房主"）
     content = `${hostname}已通过你的换宿申请，请点击下方按钮确认你将入住此房屋，并扫下方二维码支付此次换宿费用`
-    leftButtonText = '前往付款'
+    leftButtonText = '前往确认'
     rightButtonText = '前往拒绝'
   }
   
