709ad4a70a722739d4fd0a99d2e8ded3028e64da
ZIMUQIN-L <68744060+ZIMUQIN-L@users.noreply.github.com>
2025-04-02 18:46:54 +0200

    [fix]

--- src/pages/messages/index.tsx
+++ src/pages/messages/index.tsx
@@ -81,6 +81,7 @@ const Index = () => {
     const loggedIn = Boolean(GlobalStore.userInfo?.uid && GlobalStore.userInfo?.uid !== 0);
     setIsLoggedIn(loggedIn);
     if (loggedIn) {
+      GlobalStore.connectWebSocket(GlobalStore.userInfo.token);
       fetchMessages();
     }
   };
