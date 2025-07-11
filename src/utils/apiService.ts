72d8869f5c049d432c13169494b874b6cc66f276
Sakurayia <709929545@qq.com>
2025-07-11 13:56:23 +0200

    [fix] fix typo of api

--- src/utils/apiService.ts
+++ src/utils/apiService.ts
@@ -198,7 +198,7 @@ export const API = {
       images: string[];
       anonymous: boolean;
     }) =>
-      apiRequest<any>('app/propert/postPropertyReview', {
+      apiRequest<any>('app/property/postPropertyReview', {
         data
       }),
     applyProperty: (data: {
